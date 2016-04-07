8d4da8c9c382b77e5617e5f13f74427fa62234e1
Dmitry Kh <hhspms@gmail.com>
2016-04-07 15:28:51 +0200

    Solution jusqua Impulsion

--- session-2016-03-idf/3_objets/theorie/ex.py
+++ session-2016-03-idf/3_objets/theorie/ex.py
@@ -12,15 +12,16 @@ class FonctionLineaire(object):
         return self.a * x + self.b
     
 fois5plus4 = FonctionLineaire(5,4)
-print(list(map(fois5plus4,[1,5,10,20,50])))
+#print(list(map(fois5plus4,[1,5,10,20,50])))
 
 
 
 
 class Vecteur(object):
-    def __init__(self,u=0,v=0):
+    def __init__(self,u=0,v=0,w=0):
         self.__x = u
         self.__y = v
+        self.__z = w
         
     def __add__(self, other):
         if isinstance(other, Vecteur):
@@ -33,34 +34,59 @@ class Vecteur(object):
           newx = self.__x * other.__x
           newy = self.__y * other.__y
           return Vecteur(newx, newy)
+ 
+    def __getitem__(self, index):
+        coords = [self.__x,  self.__y,  self.__z]
+        return coords[index]
   
+ 
     def __str__(self):
-        return "({:.0f},{:.0f})".format(self.__x, self.__y)
+        return "({:.0f},{:.0f},{:.0f})".\
+           format(self.__x, self.__y,  self.__z)
 
 
-v1 = Vecteur(8, 3)
-v2 = Vecteur(-1, 7)
-v3 = v1 * v2
-
-print(v3)
-
+class Movement(object):
+    def __init__(self,x=0,y=0,z=0,m=0):
+        self._vitesse = Vecteur(x,y,z)
+        self._masse = m
 
 
+    def __str__(self):
+        return "{}|{:d}".format(self._vitesse.__str__(), self._masse)
 
 
+    def __eq__(self,other): 
+        return ( self._vitesse==other._vitesse ) and ( self._masse==other._masse )
+
+
+class Impulsion(Movement):
+    def __getitem__(self, index):
+        return self._masse * self._vitesse[index]
+         
+    
+
+imp = Impulsion(12,22,2,5)
+
+print(imp)
+
+print ('Imp_z', imp[2] )
+
+for i in imp:
+    print (i) 
 
 
 """
-      def __getitem__(self, index):
-        coords = [self.__x,  self.__y]
-        return coords[index]
-  
-
-print (v3[0],v3[1],v3[:], v3[1:])
-
-for i,c in enumerate(v3):
-    print ('Coordinate %d: %.1f' % (i,c))
 
 
 
+
+
+#v1 = Vecteur(8, 3)
+#v2 = Vecteur(-1, 7)
+#v3 = v1 * v2
+#
+#print(v3)
+
+
+   
 """
